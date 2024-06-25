8eed5016f3d0ba404b57fe70d7de3ed7488d996f
Barbiet <bogdanvinokurov2000@gmail.com>
2024-06-25 19:55:32 +0300

    1

--- goit-typescript-hw-01/src/main.ts
+++
@@ -1,9 +0,0 @@
-import "./style.css";
-
-function add(num1: number, num2: number) {
-  return num1 + num2;
-}
-const a = 10;
-const b = 20;
-console.log(typeof a);
-console.log(add(a, b));
--- src/basic/4.ts
+++ src/basic/4.ts
@@ -5,7 +5,7 @@ function showMessage(message: string) {
 function calc(num1: number, num2: number): number {
   return num1 + num2;
 }
-console.log(calc(1, 1));
+
 function customError(): string {
   throw new Error("Error");
 }
