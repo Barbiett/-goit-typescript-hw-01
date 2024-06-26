a5ecdaad1d8782b04b8a1702a0024706b18d4efa
Barbiet <bogdanvinokurov2000@gmail.com>
2024-06-27 01:27:47 +0300

    Done.

--- src/basic/6.ts
+++ src/basic/6.ts
@@ -2,7 +2,10 @@ interface User {
   name: string;
   age: number;
   email: string;
-  address?: any;
+  address?: {
+    city: string;
+    country: string;
+  };
 }
 const mango: User = {
   name: "Mango",
